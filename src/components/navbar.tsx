c4758826e056c54bc38204b59351ddaa3c60e5fc
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2024-07-06 18:22:16 +0200

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,6 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
 
     const activeClass = "py-2 px-3 dark:text-white text-white bg-blue-500 rounded md:bg-transparent md:text-blue-500 md:p-0 md:dark:text-blue-500 dark:bg-blue-800 md:dark:bg-transparent";
     const inactiveClass = "py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 md:w-auto dark:text-gray-400 md:dark:hover:text-blue-500 dark:focus:text-gray-400 dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent";
-
     const characters = variables.collections.characters;
     const weapons = variables.collections.weapons;
 
@@ -30,33 +29,6 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
         }
     }
 
-    const toastError = (
-        <div id="toast-danger" className="hidden absolute flex items-center top-30 left-1/2 transform -translate-x-1/2 p-4 mb-4 rounded-lg shadow-lg text-red-800 dark:text-red-200 bg-red-200 dark:bg-red-800" role="alert">
-            <div className="text-sm font-normal pe-2">{navbar.toastText}</div>
-            <button type="button" onClick={hideToast} className="ms-auto -mx-1.5 -my-1.5 bg-red-200 dark:bg-red-800 text-red-800 dark:text-red-200 rounded-lg focus:ring-2 focus:ring-red-800 dark:focus:ring-red-200 p-1.5 hover:bg-red-400 dark:hover:bg-red-400 inline-flex items-center justify-center h-8 w-8">
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-            </button>
-        </div>
-    );
-
-    var charactersList = characters.map((collectionName) => {
-        return (
-            <li key={collectionName}>
-                <a href={"/collection/" + collectionName} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{collectionName}</a>
-            </li>
-        );
-    });
-
-    var weaponsList = weapons.map((collectionName) => {
-        return (
-            <li key={collectionName}>
-                <a href={"/collection/" + collectionName} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{collectionName}</a>
-            </li>
-        );
-    });
-
     function heandleSearch(e: React.KeyboardEvent<HTMLInputElement>) {
         if (e.key === 'Enter') {
             var searchText = (e.target as HTMLInputElement).value.trim();
@@ -75,13 +47,50 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
         }
     }
 
-    const dropdownIcon = (
-        <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-        </svg>
-    );
+    function ToastError() {
+        return (
+            <div id="toast-danger" className="hidden absolute flex items-center top-30 left-1/2 transform -translate-x-1/2 p-4 mb-4 rounded-lg shadow-lg text-red-800 dark:text-red-200 bg-red-200 dark:bg-red-800" role="alert">
+                <div className="text-sm font-normal pe-2">{navbar.toastText}</div>
+                <button type="button" onClick={hideToast} className="ms-auto -mx-1.5 -my-1.5 bg-red-200 dark:bg-red-800 text-red-800 dark:text-red-200 rounded-lg focus:ring-2 focus:ring-red-800 dark:focus:ring-red-200 p-1.5 hover:bg-red-400 dark:hover:bg-red-400 inline-flex items-center justify-center h-8 w-8">
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                </button>
+            </div>
+        );
+    }
 
-    function HomeItem({ active }: { active: boolean }) {
+    function CharactersList({ characters }: { characters: string[] }) {
+        var charactersList = characters.map((collectionName) => {
+            return (
+                <li key={collectionName}>
+                    <a href={"/collection/" + collectionName} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{collectionName}</a>
+                </li>
+            );
+        });
+        return charactersList;
+    }
+
+    function WeaponsList({ weapons }: { weapons: string[] }) {
+        var weaponsList = weapons.map((collectionName) => {
+            return (
+                <li key={collectionName}>
+                    <a href={"/collection/" + collectionName} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">{collectionName}</a>
+                </li>
+            );
+        });
+        return weaponsList;
+    }
+
+    function DropdownIcon() {
+        return (
+            <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+            </svg>
+        );
+    }
+
+    function HomeItem({ active, activeClass, inactiveClass }: { active: boolean, activeClass: string, inactiveClass: string }) {
         var className = active ? activeClass : inactiveClass;
         var className = "block " + className;
         return (
@@ -93,7 +102,7 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
         );
     }
 
-    function CharactersItem({ active }: { active: boolean }) {
+    function CharactersItem({ active, activeClass, inactiveClass }: { active: boolean, activeClass: string, inactiveClass: string }) {
         var className = active ? activeClass : inactiveClass;
         var className = "flex items-center justify-between w-full " + className;
         const [charactersDropdownActive, setCharactersDropdownActive] = useState(false);
@@ -103,12 +112,12 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
                 <button id="charactersNavbarLink" data-dropdown-toggle="charactersNavbar"
                     className={className}>
                     {navbar.characters}
-                    {dropdownIcon}
+                    <DropdownIcon />
                 </button>
                 <div id="charactersNavbar" className={dropdownClass + (charactersDropdownActive ? "" : " hidden")} onClick={() => setCharactersDropdownActive(!charactersDropdownActive)}>
                     <ul id="charactersList" className="py-2 text-sm text-gray-700 dark:text-gray-400"
                         aria-labelledby="dropdownLargeButton">
-                        {charactersList}
+                        <CharactersList characters={characters} />
                     </ul>
                     <div className="py-2">
                         <a href={`/collection/${variables.collections.allCharacters}`}
@@ -119,7 +128,7 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
         );
     }
 
-    function WeaponsItem({ active }: { active: boolean }) {
+    function WeaponsItem({ active, activeClass, inactiveClass }: { active: boolean, activeClass: string, inactiveClass: string }) {
         var className = active ? activeClass : inactiveClass;
         var className = "flex items-center justify-between w-full " + className;
         const [weaponsDropdownActive, setWeaponsDropdownActive] = useState(false);
@@ -128,12 +137,12 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
             <li key="weapons">
                 <button id="weaponsNavbarLink" data-dropdown-toggle="weaponsNavbar" className={className} >
                     {navbar.weapons}
-                    {dropdownIcon}
+                    <DropdownIcon />
                 </button>
                 <div id="weaponsNavbar" className={dropdownClass + (weaponsDropdownActive ? "" : " hidden")} onClick={() => setWeaponsDropdownActive(!weaponsDropdownActive)}>
                     <ul id="weaponsList" className="py-2 text-sm text-gray-700 dark:text-gray-400"
                         aria-labelledby="dropdownLargeButton">
-                        {weaponsList}
+                        <WeaponsList weapons={weapons} />
                     </ul>
                     <div className="py-2">
                         <a href={`/collection/${variables.collections.allWeapons}`}
@@ -144,47 +153,53 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
         );
     }
 
-    const title = (
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-gray-400">
-                {navbar.title}
-            </span>
-        </a>
-    );
+    function Title() {
+        return (
+            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-gray-400">
+                    {navbar.title}
+                </span>
+            </a>
+        );
+    }
 
-    const listItems = (
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 bg-white dark:border-gray-700">
-            <HomeItem active = {activeItemID === "home"} />
-            <CharactersItem active = {activeItemID === "characters"} />
-            <WeaponsItem active = {activeItemID === "weapons"} />
-        </ul>
-    );
+    function ListItems() {
+        return (
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 bg-white dark:border-gray-700">
+                <HomeItem active = {activeItemID === "home"} activeClass = {activeClass} inactiveClass = {inactiveClass} />
+                <CharactersItem active = {activeItemID === "characters"} activeClass = {activeClass} inactiveClass = {inactiveClass} />
+                <WeaponsItem active = {activeItemID === "weapons"} activeClass = {activeClass} inactiveClass = {inactiveClass} />
+            </ul>
+        );
+    }
 
-    const searchItem = (
-        <div className="relative flex">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
+    function SearchItem() {
+        return (
+            <div className="relative flex">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="text" onKeyDown={heandleSearch}
+                    className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder={navbar.searchPlaceholder}>
+                </input>
             </div>
-            <input type="text" onKeyDown={heandleSearch}
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder={navbar.searchPlaceholder}>
-            </input>
-        </div>
-    );
+        );
+    }
 
     function DesktopNavbar() {
         return (
             <>
             <nav className="w-full mx-auto max-w-screen-xl dark:bg-gray-800 bg-white rounded-lg shadow-lg border-gray-200 m-4 bg-white">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    {title}
-                    {listItems}
-                    {searchItem}
+                    <Title />
+                    <ListItems />
+                    <SearchItem />
                 </div>
             </nav>
-            {toastError}
+            <ToastError />
         </>
         );
     }
@@ -194,7 +209,7 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
             <>
                 <nav className="w-full mx-auto max-w-screen-xl dark:bg-gray-800 bg-white rounded-lg shadow-lg border-gray-200 m-4 bg-white">
                     <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-                        {title}
+                        <Title />
                         <button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -202,11 +217,11 @@ export default function Navbar( { activeItemID }: { activeItemID: string } ) {
                         </button>
                     </div>
                     <div className="px-4 pb-4">
-                        {searchItem}
-                        {listItems}
+                        <SearchItem />
+                        <ListItems />
                     </div>
                 </nav>
-                {toastError}
+                <ToastError />
             </>
         );
     }

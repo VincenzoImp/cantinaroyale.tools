0b182850ec0f0e49e0fbe4a23bd569020d63aca2
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2024-07-06 22:27:40 +0200

    Update nftCard.tsx

--- src/components/nftCard.tsx
+++ src/components/nftCard.tsx
@@ -94,7 +94,7 @@ export default function NftCard({ nft, type }: { [key: string]: any, type: strin
                             <svg data-popover-target={"popover-"+talentName} className="flex-shrink-0 inline w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                             </svg>
-                            <div data-popover id={"popover-"+talentName} role="tooltip" className="absolute z-10 invisible inline-block w-64 text-sm text-gray-700 transition-opacity duration-300 bg-white rounded-lg shadow-lg opacity-0 dark:text-gray-200 dark:bg-gray-700">
+                            <div data-popover={"popover-"+talentName} role="tooltip" className="absolute z-10 invisible inline-block w-64 text-sm text-gray-700 transition-opacity duration-300 bg-white rounded-lg shadow-lg opacity-0 dark:text-gray-200 dark:bg-gray-700">
                                 <div className="px-3 py-2">
                                     {talents.keys[talentName].info}
                                 </div>

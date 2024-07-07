2b3a7301065fbb45a9159a07d860034780c0c378
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2024-07-07 21:19:45 +0200

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,6 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ]
+  ],
 };
 export default config;

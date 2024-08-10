88c4df2f1b428ac265c4204eb8ebb34ba0207e7e
VincenzoImp <61617279+VincenzoImp@users.noreply.github.com>
2024-08-10 18:44:27 +0200

    Update collectionTable.tsx

--- src/components/collection/collectionTable.tsx
+++ src/components/collection/collectionTable.tsx
@@ -27,13 +27,11 @@ import {ChevronDownIcon} from "../icon/chevronDownIcon";
 import {SearchIcon} from "../icon/searchIcon";
 import { list } from "postcss";
 
-export default function CollectionTable({ tableColumns, tableEntries, type }: { tableColumns: { uid: string, name: string, sortable: boolean }[], tableEntries: any[], type: string }) {
-    
-    function capitalize(str: string) {
-        return str.charAt(0).toUpperCase() + str.slice(1);
-    }      
-
-    type Nft = typeof tableEntries[0];
+export default function CollectionTable({ tableColumns, tableEntries, type }: { tableColumns: {
+    rangeble: any;
+    filterable: any;
+    searchable: any; uid: string, name: string, sortable: boolean 
+}[], tableEntries: any[], type: string }) {
 
     const INITIAL_ROWS_PER_PAGE = 10;
     const INITIAL_PAGE = 1;
@@ -43,26 +41,38 @@ export default function CollectionTable({ tableColumns, tableEntries, type }: { 
     const [rowsPerPage, setRowsPerPage] = React.useState(INITIAL_ROWS_PER_PAGE);
     const [page, setPage] = React.useState(INITIAL_PAGE);
 
-    const entriesUseStates: {
-        [key: string]: { column: any; value: any; setValue: React.Dispatch<React.SetStateAction<any>>; }[];
-    } = {
-        searchable: Object.values(tableColumns).filter((column: any) => column.searchable).map((column: any) => {
-            const [value, setValue] = React.useState("");
-            return { column: column.uid, value: value, setValue: setValue };
-        }),
-        sortable: Object.values(tableColumns).filter((column: any) => column.sortable).map((column: any) => {
-            const [value, setValue] = React.useState<string>("");
-            return { column: column.uid, value: value, setValue: setValue };
-        }),
-        filterable: Object.values(tableColumns).filter((column: any) => column.filterable).map((column: any) => {
-            const [value, setValue] = React.useState<string[]>([]);
-            return { column: column.uid, value: value, setValue: setValue };
-        }),
-        rangeble: Object.values(tableColumns).filter((column: any) => column.rangeble).map((column: any) => {
-            const [value, setValue] = React.useState<{ min: string, max: string }>({ min: "", max: "" });
-            return { column: column.uid, value: value, setValue: setValue };
-        }),
+    type Nft = typeof tableEntries[0];
+    type EntriesUseStates = {
+        searchable: { [key: string]: { value: string; setValue: React.Dispatch<React.SetStateAction<string>> } };
+        sortable: { [key: string]: { value: string; setValue: React.Dispatch<React.SetStateAction<string>> } };
+        filterable: { [key: string]: { value: string[]; setValue: React.Dispatch<React.SetStateAction<string[]>> } };
+        rangeble: { [key: string]: { value: { min: string; max: string }; setValue: React.Dispatch<React.SetStateAction<{ min: string; max: string }>> } };
     };
+    
+    const entriesUseStates: EntriesUseStates = {
+        searchable: {},
+        sortable: {},
+        filterable: {},
+        rangeble: {}
+    };
+    for (const column of Object.values(tableColumns)) {
+        if (column.searchable) {
+            const [value, setValue] = React.useState("");
+            entriesUseStates.searchable[column.uid] = { value: value, setValue: setValue };
+        }
+        if (column.sortable) {
+            const [value, setValue] = React.useState<string>("");
+            entriesUseStates.sortable[column.uid] = { value: value, setValue: setValue };
+        }
+        if (column.filterable) {
+            const [value, setValue] = React.useState<string[]>([]);
+            entriesUseStates.filterable[column.uid] = { value: value, setValue: setValue };
+        }
+        if (column.rangeble) {
+            const [value, setValue] = React.useState<{ min: string, max: string }>({ min: "", max: "" });
+            entriesUseStates.rangeble[column.uid] = { value: value, setValue: setValue };
+        }
+    }
 
     const selectedColumns = React.useMemo(() => {
         return tableColumns.filter((column) => visibleColumns.includes(column.uid));
@@ -70,134 +80,167 @@ export default function CollectionTable({ tableColumns, tableEntries, type }: { 
 
     const selectedEntries = React.useMemo(() => {
         let filteredEntries = [...tableEntries];
-        for (const column of Object.values(entriesUseStates.searchable)) {
-            if (column.value !== "" && visibleColumns.includes(column.column)) {
-                filteredEntries = filteredEntries.filter((entry) => entry[column.column].toLowerCase().includes(column.value.toLowerCase()));
+        for (const column of Object.values(tableColumns)) {
+            if (column.searchable && entriesUseStates.searchable[column.uid].value !== "") {
+                filteredEntries = filteredEntries.filter((entry) => {
+                    return entry[column.uid].includes(entriesUseStates.searchable[column.uid].value);
+                });
             }
-        }
-        for (const column of Object.values(entriesUseStates.filterable)) {
-            if (column.value.length !== 0 && visibleColumns.includes(column.column)) {
-                filteredEntries = filteredEntries.filter((entry) => column.value.includes(entry[column.column]));
+            if (column.filterable && entriesUseStates.filterable[column.uid].value.length > 0) {
+                filteredEntries = filteredEntries.filter((entry) => {
+                    return entriesUseStates.filterable[column.uid].value.includes(entry[column.uid]);
+                });
             }
-        }
-        for (const column of Object.values(entriesUseStates.rangeble)) {
-            if (column.value.min !== "" && column.value.max !== "" && visibleColumns.includes(column.column)) {
-                filteredEntries = filteredEntries.filter((entry) => entry[column.column] >= column.value.min && entry[column.column] <= column.value.max);
+            if (column.rangeble && entriesUseStates.rangeble[column.uid].value.min !== "" && entriesUseStates.rangeble[column.uid].value.max !== "") {
+                filteredEntries = filteredEntries.filter((entry) => {
+                    return (
+                        Number(entry[column.uid]) >= Number(entriesUseStates.rangeble[column.uid].value.min) &&
+                        Number(entry[column.uid]) <= Number(entriesUseStates.rangeble[column.uid].value.max)
+                    );
+                });
             }
-        }
-        for (const column of Object.values(entriesUseStates.sortable)) {
-            if (['asc', 'desc'].includes(column.value) && visibleColumns.includes(column.column)) {
-                filteredEntries = filteredEntries.sort((a: Nft, b: Nft) => {
-                    const first = a[column.column] as number;
-                    const second = b[column.column] as number;
-                    const cmp = first < second ? -1 : first > second ? 1 : 0;
-                    return column.value === "desc" ? -cmp : cmp;
+            if (column.sortable && entriesUseStates.sortable[column.uid].value !== "") {
+                filteredEntries = filteredEntries.sort((a, b) => {
+                    if (entriesUseStates.sortable[column.uid].value === "asc") {
+                        return Number(a[column.uid]) - Number(b[column.uid]);
+                    } else if (entriesUseStates.sortable[column.uid].value === "desc") {
+                        return Number(b[column.uid]) - Number(a[column.uid]);
+                    }
+                    return 0;
                 });
             }
         }
         return filteredEntries;
-    }, [tableEntries, entriesUseStates, visibleColumns]);
-    
-    const handleSort = (column: string, value: string) => {
-        for (const columnState of Object.values(entriesUseStates.sortable)) {
-            if (columnState.column === column) {
-                columnState.setValue(value);
-            }
-        }
-    };
+    }, [tableEntries, tableColumns, entriesUseStates]);
 
     const handleSearch = (column: string, value: string) => {
-        for (const columnState of Object.values(entriesUseStates.searchable)) {
-            if (columnState.column === column) {
-                columnState.setValue(value);
-            }
-        }
+        entriesUseStates.searchable[column].setValue(value);
+    };
+
+    const handleSort = (column: string, value: string) => {
+        entriesUseStates.sortable[column].setValue(value);
     };
 
     const handleFilterAdd = (column: string, value: string) => {
-        for (const columnState of Object.values(entriesUseStates.filterable)) {
-            if (columnState.column === column) {
-                columnState.setValue([...columnState.value, value]);
-            }
-        }
+        entriesUseStates.filterable[column].setValue([...entriesUseStates.filterable[column].value, value]);
     }
 
     const handleFilterRemove = (column: string, value: string) => {
-        for (const columnState of Object.values(entriesUseStates.filterable)) {
-            if (columnState.column === column) {
-                columnState.setValue(columnState.value.filter((val: string) => val !== value));
-            }
-        }
+        entriesUseStates.filterable[column].setValue(entriesUseStates.filterable[column].value.filter((v) => v !== value));
     }
 
     const handleRange = (column: string, value: { min: string, max: string }) => {
-        for (const columnState of Object.values(entriesUseStates.rangeble)) {
-            if (columnState.column === column) {
-                columnState.setValue(value);
-            }
-        }
-    }
-
-    const handleColumnAdd = (column: string) => {
-        setVisibleColumns([...visibleColumns, column]);
-    }
-
-    const handleColumnRemove = (column: string) => {
-        setVisibleColumns(visibleColumns.filter((col) => col !== column));
+        entriesUseStates.rangeble[column].setValue(value);
     }
 
     const handlePageChange = (page: number) => {
         setPage(page);
-    }
+    };
 
     const handleRowsPerPageChange = (rowsPerPage: number) => {
         setRowsPerPage(rowsPerPage);
+    };
+
+    const handleColumnVisibilityAdd = (column: string) => {
+        setVisibleColumns([...visibleColumns, column]);
+    }
+
+    const handleColumnVisibilityRemove = (column: string) => {
+        setVisibleColumns(visibleColumns.filter((c) => c !== column));
     }
 
     return (
-        <div className="overflow-x-auto">
+        <div>
             <Table>
                 <TableHeader>
-                    {selectedColumns.map((column) => {
-                        return (
-                            <TableColumn key={column.uid}>
+                    {tableColumns.map((column) => (
+                        <TableColumn key={column.uid}>
+                            {column.name}
+                            {column.searchable && (
+                                <Input
+                                    placeholder="Search"
+                                    value={entriesUseStates.searchable[column.uid].value}
+                                    onChange={(e) => handleSearch(column.uid, e.target.value)}
+                                />
+                            )}
+                            {column.sortable && (
+                                <Button
+                                    variant="ghost"
+                                    onClick={() => handleSort(column.uid, "asc")}
+                                />
+                            )}
+                            {column.sortable && (
+                                <Button
+                                    variant="ghost"
+                                    onClick={() => handleSort(column.uid, "desc")}
+                                />
+                            )}
+                            {column.filterable && (
                                 <Dropdown>
                                     <DropdownTrigger>
-                                        <Button size="sm" variant="ghost">
-                                            {column.name}
-                                            <ChevronDownIcon />
-                                        </Button>
+                                        <Button variant="ghost" />
                                     </DropdownTrigger>
                                     <DropdownMenu>
-                                        <DropdownItem onClick={() => handleSort(column.uid, "asc")}>Ascending</DropdownItem>
-                                        <DropdownItem onClick={() => handleSort(column.uid, "desc")}>Descending</DropdownItem>
+                                        {tableEntries.map((entry) => (
+                                            <DropdownItem
+                                                key={entry[column.uid]}
+                                                onClick={() => handleFilterAdd(column.uid, entry[column.uid])}
+                                            >
+                                                {entry[column.uid]}
+                                            </DropdownItem>
+                                        ))}
                                     </DropdownMenu>
                                 </Dropdown>
-                            </TableColumn>
-                        );
-                    })}
+                            )}
+                            {column.filterable && (
+                                entriesUseStates.filterable[column.uid].value.map((value) => (
+                                    <Chip
+                                        key={value}
+                                        onClose={() => handleFilterRemove(column.uid, value)}
+                                    >
+                                        {value}
+                                    </Chip>
+                                ))
+                            )}
+                            {column.rangeble && (
+                                <Input
+                                    placeholder="Min"
+                                    value={entriesUseStates.rangeble[column.uid].value.min}
+                                    onChange={(e) => handleRange(column.uid, { min: e.target.value, max: entriesUseStates.rangeble[column.uid].value.max })}
+                                />
+                            )}
+                            {column.rangeble && (
+                                <Input
+                                    placeholder="Max"
+                                    value={entriesUseStates.rangeble[column.uid].value.max}
+                                    onChange={(e) => handleRange(column.uid, { min: entriesUseStates.rangeble[column.uid].value.min, max: e.target.value })}
+                                />
+                            )}
+                            <Button
+                                variant="ghost"
+                                onClick={() => handleColumnVisibilityAdd(column.uid)}
+                            />
+                        </TableColumn>
+                    ))}
                 </TableHeader>
                 <TableBody>
-                    {selectedEntries.slice((page - 1) * rowsPerPage, page * rowsPerPage).map((entry, index) => {
-                        return (
-                            <TableRow key={index}>
-                                {selectedColumns.map((column) => {
-                                    return (
-                                        <TableCell key={column.uid}>
-                                            {entry[column.uid]}
-                                        </TableCell>
-                                    );
-                                })}
-                            </TableRow>
-                        );
-                    })}
+                    {selectedEntries.slice((page - 1) * rowsPerPage, page * rowsPerPage).map((entry) => (
+                        <TableRow key={entry.id}>
+                            {selectedColumns.map((column) => (
+                                <TableCell key={column.uid}>
+                                    {entry[column.uid]}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
             <Pagination
-                total={selectedEntries.length}
+                total={tableEntries.length}
                 page={page}
                 onChange={handlePageChange}
             />
         </div>
     );
+
 }
